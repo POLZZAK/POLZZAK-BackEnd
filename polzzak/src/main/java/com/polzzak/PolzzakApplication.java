15e8963fde67ad712f5dfed63d39ebc64f0a6e9a
jihoon-jang <jigns153@naver.com>
2023-04-22 14:19:26 +0900

    프로젝트 초기 세팅

---
+++ polzzak/src/main/java/com/polzzak/PolzzakApplication.java
@@ -0,0 +1,13 @@
+package com.polzzak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolzzakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolzzakApplication.class, args);
+	}
+
+}
---
+++ polzzak/src/test/java/com/polzzak/PolzzakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polzzak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolzzakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
