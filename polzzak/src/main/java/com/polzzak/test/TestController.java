430b1ad57c131a31d6bed7371f205e8ae8494ced
jihoon-jang <jigns153@naver.com>
2023-04-24 13:20:26 +0900

    패키지 경로 수정

--- polzzak/src/main/java/com/polzzak/test/TestController.java
+++ polzzak/src/main/java/com/polzzak/test/TestController.java
@@ -1,5 +1,6 @@
-package com.test;
+package com.polzzak.test;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
 
     @GetMapping("/a")
-    public String test() {
-        return "HIHI";
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("HIHI");
     }
 }
