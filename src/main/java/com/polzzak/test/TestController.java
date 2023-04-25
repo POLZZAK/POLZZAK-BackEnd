ed3caf07b145e2ad8edb9cb284aa947f8b12b6bf
jihoon-jang <jigns153@naver.com>
2023-04-25 14:39:18 +0900

    배포 관련 설정 변경
    오타 수정

--- src/main/java/com/polzzak/test/TestController.java
+++ src/main/java/com/polzzak/test/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/a")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("HIHI Hello HIHI new Deploy from cicd");
+        return ResponseEntity.ok("new Hello");
     }
 }
