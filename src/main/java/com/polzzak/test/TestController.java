7a5194ce9994fe05365046819aa05a2304a7f61e
jihoon-jang <jigns153@naver.com>
2023-04-28 00:57:57 +0900

    rest docs 추가

--- src/main/java/com/polzzak/test/TestController.java
+++ src/main/java/com/polzzak/test/TestController.java
@@ -3,6 +3,10 @@ package com.polzzak.test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class TestController {
@@ -10,5 +14,11 @@ public class TestController {
     @GetMapping("/a")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("new Hello");
+    }
+
+    @GetMapping("/{path}/rest")
+    public ResponseEntity<Map<String, Object>> rest(@PathVariable String path, @RequestParam int param) {
+        Map<String, Object> data = Map.of("path", path, "param", param);
+        return ResponseEntity.ok(data);
     }
 }
