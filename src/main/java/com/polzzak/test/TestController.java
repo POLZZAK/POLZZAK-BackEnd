d1135ad75891b08b32921052648927fa2a3d02c8
jihoon-jang <jigns153@naver.com>
2023-04-28 17:44:06 +0900

    static file print api 추가

--- src/main/java/com/polzzak/test/TestController.java
+++ src/main/java/com/polzzak/test/TestController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -20,5 +23,27 @@ public class TestController {
     public ResponseEntity<Map<String, Object>> rest(@PathVariable String path, @RequestParam int param) {
         Map<String, Object> data = Map.of("path", path, "param", param);
         return ResponseEntity.ok(data);
+    }
+
+    @GetMapping("/static/files")
+    public ResponseEntity<List<String>> getStaticFiles() {
+        List<String> fileList = new ArrayList<>();
+
+        // static 디렉토리 경로를 구합니다.
+        String path = getClass().getResource("/static").getPath();
+        File directory = new File(path);
+
+        // static 디렉토리 내의 모든 파일과 디렉토리 목록을 구합니다.
+        for (File file : directory.listFiles()) {
+            fileList.add(file.getName());
+            if (file.isDirectory()) {
+                for (File subFile : file.listFiles()) {
+                    fileList.add(file.getName() + "/" + subFile.getName());
+                }
+            }
+        }
+
+
+        return ResponseEntity.ok(fileList);
     }
 }
