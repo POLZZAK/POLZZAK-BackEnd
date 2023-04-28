d445ed42c14c95fd04b2d178dbd2ea0942f094e1
jihoon-jang <jigns153@naver.com>
2023-04-28 18:54:32 +0900

    경로 수정

--- src/main/java/com/polzzak/test/TestController.java
+++ src/main/java/com/polzzak/test/TestController.java
@@ -27,33 +27,37 @@ public class TestController {
     }
 
     @GetMapping("/static/files")
-    public ResponseEntity<?> getStaticFiles() {    String rootPath = "src"; // 루트 경로
-        File rootDirectory = new File(rootPath); // 루트 디렉토리 객체 생성
+    public ResponseEntity<?> getStaticFiles() {
+        String rootPath = "."; // 실행 디렉토리를 루트 디렉토리로 설정
+        File rootDirectory = new File(rootPath);
         List<String> fileList = new ArrayList<>();
 
-        // 루트 디렉토리부터 하위 디렉토리 및 파일을 재귀적으로 순회하며 파일 목록 생성
         createFileList(rootDirectory, "", fileList);
-        String a= "";
+
+        String a = "";
 
         for (String s : fileList) {
             a = a + s + "</br>";
         }
 
         System.out.println(a);
-        return ResponseEntity.ok(a); // 뷰 이름 반환
+        return ResponseEntity.ok(a);
     }
 
     private void createFileList(File directory, String prefix, List<String> fileList) {
-        File[] files = directory.listFiles(); // 디렉토리 내 파일 목록 조회
+        File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
-                if (file.isDirectory()) { // 디렉토리일 경우
+                if (file.isDirectory()) {
                     String directoryName = file.getName();
-                    createFileList(file, prefix + "  ", fileList); // 하위 디렉토리 순회
-                } else { // 파일일 경우
+//                    fileList.add(prefix + directoryName + "/");
+                    createFileList(file, prefix + "  ", fileList);
+                } else {
                     String fileName = file.getName();
-                    String filePath = file.getAbsolutePath(); // 파일의 절대 경로 조회
-                    System.out.println(filePath);
+                    String filePath = file.getAbsolutePath();
+                    String relativePath = new File("").getAbsolutePath(); // 실행 디렉토리
+                    relativePath = relativePath.substring(0, relativePath.length() - 1); // 끝에 "/" 제거
+                    filePath = filePath.replace(relativePath, ""); // 실행 디렉토리 경로 제거
                     fileList.add(prefix  + " (" + filePath + ")");
                 }
             }
