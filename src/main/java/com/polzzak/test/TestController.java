c0b7deeb691e00c4931f49ae65fc3941095c1a48
jihoon-jang <jigns153@naver.com>
2023-04-28 18:29:59 +0900

    프로젝트 파일 출력 API

--- src/main/java/com/polzzak/test/TestController.java
+++ src/main/java/com/polzzak/test/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,24 +27,35 @@ public class TestController {
     }
 
     @GetMapping("/static/files")
-    public ResponseEntity<List<String>> getStaticFiles() {
+    public ResponseEntity<?> getStaticFiles() {    String rootPath = "src/main"; // 루트 경로
+        File rootDirectory = new File(rootPath); // 루트 디렉토리 객체 생성
         List<String> fileList = new ArrayList<>();
 
-        // static 디렉토리 경로를 구합니다.
-        String path = getClass().getResource("/static").getPath();
-        File directory = new File(path);
+        // 루트 디렉토리부터 하위 디렉토리 및 파일을 재귀적으로 순회하며 파일 목록 생성
+        createFileList(rootDirectory, "", fileList);
+        String a= "";
 
-        // static 디렉토리 내의 모든 파일과 디렉토리 목록을 구합니다.
-        for (File file : directory.listFiles()) {
-            fileList.add(file.getName());
-            if (file.isDirectory()) {
-                for (File subFile : file.listFiles()) {
-                    fileList.add(file.getName() + "/" + subFile.getName());
+        for (String s : fileList) {
+            a = a + s + "</br>";
+        }
+
+        System.out.println(a);
+        return ResponseEntity.ok(a); // 뷰 이름 반환
+    }
+
+    private void createFileList(File directory, String prefix, List<String> fileList) {
+        File[] files = directory.listFiles(); // 디렉토리 내 파일 목록 조회
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) { // 디렉토리일 경우
+                    String directoryName = file.getName();
+                    createFileList(file, prefix + "  ", fileList); // 하위 디렉토리 순회
+                } else { // 파일일 경우
+                    String fileName = file.getName();
+                    String filePath = file.getAbsolutePath(); // 파일의 절대 경로 조회
+                    fileList.add(prefix  + " (" + filePath + ")");
                 }
             }
         }
-
-
-        return ResponseEntity.ok(fileList);
     }
 }
