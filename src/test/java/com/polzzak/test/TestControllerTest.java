808f49504b316935ebc67a31f3e99e3832f1a606
YC <gks54410@gmail.com>
2023-04-30 17:41:36 +0900

    test: controller test 구체화

--- src/test/java/com/polzzak/test/TestControllerTest.java
+++ src/test/java/com/polzzak/test/TestControllerTest.java
@@ -1,55 +1,40 @@
 package com.polzzak.test;
 
+import com.polzzak.support.test.ControllerTestHelper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @WebMvcTest(TestController.class)
-@AutoConfigureRestDocs
-@AutoConfigureMockMvc
-class TestControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
+class TestControllerTest extends ControllerTestHelper {
 
     @Test
     public void example() throws Exception {
         mockMvc.perform(
-                        get("/{path}/rest", "path_value")
-                                .param("param", "123")
-                                .contentType(MediaType.APPLICATION_JSON)
+                get("/{path}/rest", "path_value")
+                    .param("param", "123")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andDo(
+                document("example-get",
+                    pathParameters(
+                        parameterWithName("path").description("path 값")
+                    ),
+                    queryParameters(parameterWithName("param").description("param")),
+                    responseFields(
+                        fieldWithPath("path").description("path"),
+                        fieldWithPath("dir").description("dir"),
+                        fieldWithPath("param").description("param")
+                    )
                 )
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andDo(
-                        document("example-get",
-                                pathParameters(
-                                        parameterWithName("path").description("path 값")
-                                ),
-                                queryParameters(parameterWithName("param").description("param")),
-                                responseFields( // response 필드 정보 입력
-                                        fieldWithPath("path").description("path"),
-                                        fieldWithPath("dir").description("dir"),
-                                        fieldWithPath("param").description("param")
-                                )
-                        )
-                )
-        ;
+            );
     }
 }
