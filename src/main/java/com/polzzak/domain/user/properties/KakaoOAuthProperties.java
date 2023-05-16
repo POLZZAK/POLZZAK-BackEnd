e5424545ce289802ea6cd0cde3e1b147ca9a2651
YC <gks54410@gmail.com>
2023-05-16 18:39:59 +0900

    feat: TokenUrl 제거

--- src/main/java/com/polzzak/domain/user/properties/KakaoOAuthProperties.java
+++ src/main/java/com/polzzak/domain/user/properties/KakaoOAuthProperties.java
@@ -9,19 +9,12 @@ import lombok.Getter;
 public class KakaoOAuthProperties extends OAuthProperties {
 	private final String apiKey;
 	private final String secretKey;
-	private final String kakaoTokenUrl;
 	private final String kakaoUserInfoUrl;
 
-	public KakaoOAuthProperties(String apiKey, String secretKey, String kakaoTokenUrl, String kakaoUserInfoUrl) {
+	public KakaoOAuthProperties(String apiKey, String secretKey, String kakaoUserInfoUrl) {
 		this.apiKey = apiKey == null ? "" : apiKey;
 		this.secretKey = secretKey == null ? "" : secretKey;
-		this.kakaoTokenUrl = kakaoTokenUrl == null ? "" : kakaoTokenUrl;
 		this.kakaoUserInfoUrl = kakaoUserInfoUrl == null ? "" : kakaoUserInfoUrl;
-	}
-
-	@Override
-	public String getTokenUrl() {
-		return this.kakaoTokenUrl;
 	}
 
 	@Override
