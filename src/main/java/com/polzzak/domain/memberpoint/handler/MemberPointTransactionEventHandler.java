9f95dd3f1c6d255360687f04eb688e526ccd108d
YC <gks54410@gmail.com>
2023-06-29 20:19:25 +0900

    feat: 불필요한 설정 제거 및 이벤트 처리 중 예외 발생 시 로그 추가

--- src/main/java/com/polzzak/domain/memberpoint/handler/MemberPointTransactionEventHandler.java
+++ src/main/java/com/polzzak/domain/memberpoint/handler/MemberPointTransactionEventHandler.java
@@ -2,8 +2,6 @@ package com.polzzak.domain.memberpoint.handler;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.polzzak.domain.family.entity.FamilyMapCreatedEvent;
@@ -15,6 +13,9 @@ import com.polzzak.domain.stampboard.entity.StampBoardCreatedEvent;
 import com.polzzak.domain.stampboard.entity.StampBoardDeletedEvent;
 import com.polzzak.domain.stampboard.entity.StampCreatedEvent;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class MemberPointTransactionEventHandler {
 	private final MemberPointService memberPointService;
@@ -25,48 +26,60 @@ public class MemberPointTransactionEventHandler {
 
 	@Async
 	@TransactionalEventListener
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void handleStampBoardCreateEvent(final StampBoardCreatedEvent event) {
-		MemberPoint memberPoint = memberPointService.getMemberPoint(event.guardian().getId());
-		memberPoint.updatePoint(MemberPointType.STAMP_BOARD_CREATION.getIncreasedPoint());
-		memberPointService.saveMemberPointHistory(
-			createMemberPointHistory(MemberPointType.STAMP_BOARD_CREATION, memberPoint));
+	public void handleStampBoardCreatedEvent(final StampBoardCreatedEvent event) {
+		try {
+			MemberPoint memberPoint = memberPointService.getMemberPoint(event.guardian().getId());
+			memberPoint.updatePoint(MemberPointType.STAMP_BOARD_CREATION.getIncreasedPoint());
+			memberPointService.saveMemberPointHistory(
+				createMemberPointHistory(MemberPointType.STAMP_BOARD_CREATION, memberPoint));
+		} catch (RuntimeException ex) {
+			log.error("[handleStampBoardCreatedEvent]", ex);
+		}
 	}
 
 	@Async
 	@TransactionalEventListener
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void handleStampCreateEvent(final StampCreatedEvent event) {
-		event.members()
-			.forEach(member -> {
-				MemberPoint memberPoint = memberPointService.getMemberPoint(member.getId());
-				memberPoint.updatePoint(MemberPointType.STAMP_CREATION.getIncreasedPoint());
-				memberPointService.saveMemberPointHistory(
-					createMemberPointHistory(MemberPointType.STAMP_CREATION, memberPoint));
-			});
+	public void handleStampCreatedEvent(final StampCreatedEvent event) {
+		try {
+			event.members()
+				.forEach(member -> {
+					MemberPoint memberPoint = memberPointService.getMemberPoint(member.getId());
+					memberPoint.updatePoint(MemberPointType.STAMP_CREATION.getIncreasedPoint());
+					memberPointService.saveMemberPointHistory(
+						createMemberPointHistory(MemberPointType.STAMP_CREATION, memberPoint));
+				});
+		} catch (RuntimeException ex) {
+			log.error("[handleStampCreatedEvent]", ex);
+		}
 	}
 
 	@Async
 	@TransactionalEventListener
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void handleStampBoardDeleteEvent(final StampBoardDeletedEvent event) {
-		MemberPoint memberPoint = memberPointService.getMemberPoint(event.guardian().getId());
-		memberPoint.updatePoint(MemberPointType.STAMP_BOARD_REMOVAL.getIncreasedPoint());
-		memberPointService.saveMemberPointHistory(
-			createMemberPointHistory(MemberPointType.STAMP_BOARD_REMOVAL, memberPoint));
+	public void handleStampBoardDeletedEvent(final StampBoardDeletedEvent event) {
+		try {
+			MemberPoint memberPoint = memberPointService.getMemberPoint(event.guardian().getId());
+			memberPoint.updatePoint(MemberPointType.STAMP_BOARD_REMOVAL.getIncreasedPoint());
+			memberPointService.saveMemberPointHistory(
+				createMemberPointHistory(MemberPointType.STAMP_BOARD_REMOVAL, memberPoint));
+		} catch (RuntimeException ex) {
+			log.error("[handleStampBoardDeletedEvent]", ex);
+		}
 	}
 
 	@Async
 	@TransactionalEventListener
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void handleFamilyMapCreateEvent(final FamilyMapCreatedEvent event) {
-		event.members()
-			.forEach(member -> {
-				MemberPoint memberPoint = memberPointService.getMemberPoint(member.getId());
-				memberPoint.updatePoint(MemberPointType.FAMILY_MAP_CREATION.getIncreasedPoint());
-				memberPointService.saveMemberPointHistory(
-					createMemberPointHistory(MemberPointType.FAMILY_MAP_CREATION, memberPoint));
-			});
+	public void handleFamilyMapCreatedEvent(final FamilyMapCreatedEvent event) {
+		try {
+			event.members()
+				.forEach(member -> {
+					MemberPoint memberPoint = memberPointService.getMemberPoint(member.getId());
+					memberPoint.updatePoint(MemberPointType.FAMILY_MAP_CREATION.getIncreasedPoint());
+					memberPointService.saveMemberPointHistory(
+						createMemberPointHistory(MemberPointType.FAMILY_MAP_CREATION, memberPoint));
+				});
+		} catch (RuntimeException ex) {
+			log.error("[handleFamilyMapCreatedEvent]", ex);
+		}
 	}
 
 	private MemberPointHistory createMemberPointHistory(final MemberPointType pointType,
