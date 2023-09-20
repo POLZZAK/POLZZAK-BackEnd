56ff5998f82f59e2170647135475ce9828722600
YC <gks54410@gmail.com>
2023-09-20 22:39:12 +0900

    feat: 닉네임 수정 API 추가

--- src/main/java/com/polzzak/domain/user/controller/UserRestController.java
+++ src/main/java/com/polzzak/domain/user/controller/UserRestController.java
@@ -4,12 +4,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.polzzak.domain.user.dto.MemberResponse;
+import com.polzzak.domain.user.dto.UpdateNicknameRequest;
 import com.polzzak.domain.user.service.UserService;
 import com.polzzak.global.common.ApiResponse;
 import com.polzzak.global.security.LoginId;
@@ -30,13 +32,22 @@ public class UserRestController {
 	}
 
 	@PatchMapping(path = "/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ResponseEntity<ApiResponse<MemberResponse>> updateUserProfile(
+	public ResponseEntity<Void> updateUserProfile(
 		final @LoginId Long memberId,
 		final @RequestPart("profile") MultipartFile profile
 	) {
 		final String profileKey = userService.uploadProfile(profile);
 		final String prevProfileKey = userService.updateMemberProfile(memberId, profileKey);
 		userService.deleteProfile(prevProfileKey);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PatchMapping(path = "/nickname")
+	public ResponseEntity<Void> updateUserNickname(
+		final @LoginId Long memberId,
+		final @RequestBody UpdateNicknameRequest request
+	) {
+		userService.updateNickname(memberId, request);
 		return ResponseEntity.noContent().build();
 	}
 }
