729f0921f8b261505056da7d809ee0b97564ee49
jihoon-jang <jigns153@naver.com>
2023-10-02 15:52:36 +0900

    feat: 알림 설정 연동

--- src/main/java/com/polzzak/domain/notification/handler/NotificationEventHandler.java
+++ src/main/java/com/polzzak/domain/notification/handler/NotificationEventHandler.java
@@ -3,9 +3,9 @@ package com.polzzak.domain.notification.handler;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.polzzak.domain.notification.dto.NotificationCreateEvent;
+import com.polzzak.domain.notification.dto.NotificationSettingDto;
 import com.polzzak.domain.notification.entity.Notification;
 import com.polzzak.domain.notification.entity.NotificationType;
 import com.polzzak.domain.notification.service.NotificationService;
@@ -34,6 +34,63 @@ public class NotificationEventHandler {
 				//기존 알림 타입 변경용 타입
 				return;
 			}
+
+			NotificationSettingDto notificationSetting = notificationService.getNotificationSetting(event.receiverId());
+			switch (event.type()) {
+				case FAMILY_REQUEST, FAMILY_REQUEST_COMPLETE -> {
+					if (!notificationSetting.familyRequest()) {
+						return;
+					}
+				}
+				case LEVEL_UP, LEVEL_DOWN -> {
+					if (!notificationSetting.level()) {
+						return;
+					}
+				}
+				case STAMP_REQUEST -> {
+					if (!notificationSetting.stampRequest()) {
+						return;
+					}
+				}
+				case REWARD_REQUEST -> {
+					if (!notificationSetting.rewardRequest()) {
+						return;
+					}
+				}
+				case STAMP_BOARD_COMPLETE -> {
+					if (!notificationSetting.stampBoardComplete()) {
+						return;
+					}
+				}
+				case REWARDED -> {
+					if (!notificationSetting.rewarded()) {
+						return;
+					}
+				}
+				case REWARD_FAIL -> {
+					if (!notificationSetting.rewardFail()) {
+						return;
+					}
+				}
+				case CREATED_STAMP_BOARD -> {
+					if (!notificationSetting.createdStampBoard()) {
+						return;
+					}
+				}
+				case ISSUED_COUPON -> {
+					if (!notificationSetting.issuedCoupon()) {
+						return;
+					}
+				}
+				case REWARDED_REQUEST -> {
+					if (!notificationSetting.rewardedRequest()) {
+						return;
+					}
+				}
+				default -> {
+				}
+			}
+
 			notificationService.addNotification(event.senderId(), event.receiverId(), event.type(), event.data());
 
 			log.info("[NotificationEvent] info. sender_id : {}, receiver_id : {}, type : {}, data : {}",
